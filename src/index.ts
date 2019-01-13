fc30633aaa1f300fcc32be3d18a6e63c8650b0a2
Jason Scherer <schereja@gmail.com>
2019-01-12 22:37:34 -0600

    Addition of initial view Addition of npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+
+dotenv.config();
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.SERVER_PORT; // default port to listen
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.render("index");
 } );
 
 // start the Express server
